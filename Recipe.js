caf505f2b85bca2b82c03ab1ec889a795e110284
tintu mariyam <tintumpaul01@gmail.com>
2024-12-05 17:21:10 +0000

    front end correction implementation

--- Recipe.js
+++ Recipe.js
@@ -1,28 +1,33 @@
-const apiUrl = 'http://localhost:3000';
+const apiUrl = 'http://localhost:3306';
+
 function displayrecipies() {
     fetch(apiUrl)
         .then(response => response.json())
         .then(recipe => {
             const recipe_List = document.getElementById('recipelist');
             recipe_List.innerHTML = ''; 
-    recipe_List.innerHTML = '';
 
-    recipe.forEach((recipes,index) => {
-        const result = document.createElement('div');
-        result.innerHTML = `
-        ${recipes.title} - 
-        ${recipes.category} - 
-        ${recipes.ingredients} - 
-        ${recipes.steps} - 
-        ${recipes.cookingTime} min - 
-        ${recipes.cookingMethod} - 
-        ${recipes.spiceLevel}
-        <button onclick ="deleteRecipe(${index})">Delete</button>
+        recipe.forEach(recipes => {
+            const result = document.createElement('div');
+            result.innerHTML = `
+            ${recipes.title} - 
+            ${recipes.category} - 
+            ${recipes.ingredients} - 
+            ${recipes.steps} - 
+            ${recipes.cookingTime} min - 
+            ${recipes.cookingMethod} - 
+            ${recipes.spiceLevel}
+            <button onclick ="deleteRecipe(${index})">Delete</button>
     `;
     recipe_List = appendchild(result);
     
-    });
-});
+        });
+    })
+      .catch(err => {
+        console.error('Error fetching recipes:', err);
+      });
+}
+
 
 function addrecipe() {  
     
@@ -39,6 +44,20 @@ function addrecipe() {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(newRecipe)
+    })
+    .then(() => displayrecipies())
+    .catch(err => {
+        console.error('Error adding recipe:', err);
+    });
+}
 
-       .then(() => displayrecipies()) 
-    }) 
+function deleteRecipe(id) {
+        fetch(`${apiUrl}/${id}`, {
+            method: 'DELETE'
+        })
+        .then(() => displayrecipies())
+        .catch(err => {
+            console.error('Error deleting recipe:', err);
+        });
+}
+displayrecipies();
